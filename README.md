e046c23a8dc99e6f58e3a2c68ea5fca165b7b9c2
Rakesh Gilikenahalli <rakesh@server.com>
2012-07-03 00:09:27 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This project helps to get data of any Twitter account using a perl script. The perl script uses modules : Net::Twitter module and Data::Dumper. 
+
+1. You would need to have an account in Twitter to get the details required in get_data.conf file.
+2. Edit file get_data.conf.template with the details thus acquired and copy it to get_data.conf and then run ./get_data.pl. 
+3. Getting an account and getting details should be a matter of 5-10 minutes.
+4. If the perl modules(mentioned above) are not installed on your computer, use command "cpanm Module_name" in your command line to install. 
---
+++ get_data.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl
+use Net::Twitter;
+use Data::Dumper;
+use Getopt::Long;
+
+my $handle = "arbit";
+my $consumer_key = "arbit";
+my $consumer_secret = "arbit";
+my $access_token = "arbit";
+my $access_token_secret = "arbit";
+my $file_name = "get_data.conf";
+
+open ($CONF_FILE, "<$file_name");
+
+while (<$CONF_FILE>) {
+ 	chomp;
+	if(/handle\s*:\s*/) {
+		$_ =~ s/\s*//g;
+		($handle = $_) =~ s/handle\://;
+	}
+	elsif(/consumer_key\s*:\s*/) {
+		$_ =~ s/\s*//g;
+		($consumer_key = $_) =~ s/consumer_key://;
+	}
+	elsif(/consumer_secret\s*:\s*/) {
+		$_ =~ s/\s*//g;
+		($consumer_secret = $_) =~ s/consumer_secret://;
+	}
+	elsif(/access_token\s*:\s*/) {
+		$_ =~ s/\s*//g;
+		($access_token = $_) =~ s/access_token://;
+	}
+	elsif(/access_token_secret\s*:\s*/) {
+		$_ =~ s/\s*//g;
+		($access_token_secret = $_) =~ s/access_token_secret://;
+	}
+}
+close ($CONF_FILE); 
+
+#print Dumper($handle);
+#print Dumper($consumer_key);
+#print Dumper($consumer_secret);
+#print Dumper($access_token);
+#print Dumper($access_token_secret);
+
+my $nt = Net::Twitter->new(
+  consumer_key        => $consumer_key,
+  consumer_secret     => $consumer_secret,
+  access_token        => $access_token,
+  access_token_secret => $access_token_secret,
+  legacy_lists_api    => 0 
+);
+
+my @ids;
+
+for ( my $cursor = -1, my $r; $cursor; $cursor = $r->{next_cursor} ) {
+	$r = $nt->followers_ids({ cursor => $cursor , screen_name => $handle });
+	push @ids, @{ $r->{ids} };
+        #sleep 240;
+}
+
+my $num_ids_in_array = @ids;
+print "Number of followers = $num_ids_in_array\n";
+print "Array of ids start:\n @ids \nArray of ids ends\n";
+
+while(@ids) {
+	my @ids_100 = splice @ids,0,100;
+        # get a result set of users
+        my $friends = $nt->lookup_users({user_id=>\@ids_100});
+
+        # Loop over the array ref to get each user?
+        foreach (@$friends) {
+        	my $user = $_;
+        	my $screen_name = $user->{"screen_name"};
+        	print Dumper($user);
+	}
+	#sleep 30;
+}
+
+my $num_api_acc_left = $nt->rate_limit_status->{remaining_hits};
+print "Number of api acc left = $num_api_acc_left\n";
